af4952cf9d0748b51fc6439164e6795ac73588c1
Kumaran Arulmani <kums220@gmail.com>
2016-05-10 14:39:51 -0500

    Test JFile Upload
    disregard

---
+++ InsertionSort.java
@@ -0,0 +1,45 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class - 
+//Lab  -
+
+import java.util.ArrayList;
+import java.util.Collections;
+import static java.lang.System.*;
+
+public class InsertionSort
+{
+	private ArrayList<String> list;
+
+	public InsertionSort()
+	{
+		list = new ArrayList<String>(0);
+
+	}
+
+	//modfiers
+	public void add(String  word)
+	{
+		for(String i : list)
+		{
+			if(i.equals(word))
+				break;
+		}
+		list.add(word);
+		Collections.sort(list);
+
+	}
+
+
+	public void remove(String word)
+	{
+		list.remove(word);
+
+	}
+
+	public String toString( )
+	{
+		return list.toString().replaceAll("[\\[\\]]","").replaceAll(", "," ");
+	}
+}
